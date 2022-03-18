e9606ebaeec03a82f9eaef55bd46f5d0e3cf0d73
Odell Davis III <odell3@gmail.com>
2022-03-18 13:54:55 -0400

    Coding Problems

---
+++ CodingProblems/Leetcode/MeetingRoomsI.java
@@ -0,0 +1,31 @@
+/*
+Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
+
+Example 1:
+
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: false
+
+Example 2:
+
+Input: intervals = [[7,10],[2,4]]
+Output: true
+ 
+
+Constraints:
+
+0 <= intervals.length <= 10^4
+intervals[i].length == 2
+0 <= starti < endi <= 10^6
+*/
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i][1] > intervals[i + 1][0]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
